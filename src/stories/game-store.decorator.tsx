9708ab319af1cd088d9f74777d9f9f6620859c3d
Shengtian Liao <tigerbook73@outlook.com>
2025-12-04 21:22:31 +1100

    feat: refactor GameStoreDecorator to use Game.fromJSON for game initialization

--- src/stories/game-store.decorator.tsx
+++ src/stories/game-store.decorator.tsx
@@ -5,7 +5,7 @@ import { getFakeEvent } from "@/test/helper";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const GameStoreDecorator = (Story: any) => {
   const setGame = useGameStore((state) => state.setGame);
-  const game = getFakeEvent().data.rooms[0].game as unknown as Game;
+  const game = Game.fromJSON(getFakeEvent().data.rooms[0].game);
   setGame(game);
 
   return <Story />;
