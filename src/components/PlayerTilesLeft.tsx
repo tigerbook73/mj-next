aa214c80997f06f26a51ee0e21dd9c3c52b7a46c
Liao Shengtian <liaoshengtian@hotmail.com>
2026-02-20 23:40:54 +1100

    feat: Highlight current player in PlayerTiles components and add useIsCurrentPlayer hook (#4)

--- src/components/PlayerTilesLeft.tsx
+++ src/components/PlayerTilesLeft.tsx
@@ -1,10 +1,21 @@
+"use client";
+
 import { HandTiles } from "./HandTiles";
 import { OpenSetTiles } from "./OpenSetTiles";
 import { Direction } from "@/lib/game-utils";
+import { useIsCurrentPlayer } from "@/hooks/useIsCurrentPlayer";
+import { cn } from "@/lib/utils";
 
 export function PlayerTilesLeft() {
+  const isCurrentPlayer = useIsCurrentPlayer(Direction.Left);
+
   return (
-    <div className="flex h-full w-full flex-col items-center justify-between">
+    <div
+      className={cn(
+        "flex h-full w-full flex-col items-center justify-between transition-colors duration-300",
+        isCurrentPlayer && "bg-amber-400/10 ring-2 ring-inset ring-amber-400/60",
+      )}
+    >
       <OpenSetTiles direction={Direction.Left} />
       <HandTiles direction={Direction.Left} className="flex-1" />
     </div>
